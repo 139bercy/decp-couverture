0f4fbec9b90745fb702ad29d25320157cdd998ad
pauldes <paul.desigaud@wavestone.com>
2021-11-17 12:41:22 +0100

    Initialisation de streamlit

---
+++ streamlit_app.py
@@ -0,0 +1,7 @@
+"""Ce fichier sert au déploiement sur streamlit.io. Il permet de lancer
+l'application Web depuis la racine du projet avec le CLI Streamlit (streamlit run)
+"""
+
+from decp_couverture import web
+
+web.app.run()
