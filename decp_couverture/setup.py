2fe5928db698abb0920be9cc14da608557d93cf6
pauldes <paul.desigaud@wavestone.com>
2021-11-17 12:37:08 +0100

    Initialisation du module decp_couverture

---
+++ decp_couverture/setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+
+setup(
+    name="decp-couverture",
+    version="0.1",
+    description="Couverture géographique des Données Essentielles de la Commande Publique (DECP).",
+)
